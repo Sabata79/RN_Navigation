6d8725927dfaa8fcff224387887b2c3d174067f1
Sasa Mora Roca <sabata79@hotmail.com>
2023-09-19 13:21:28 +0300

    Navigate training

--- App.js
+++ App.js
@@ -1,12 +1,88 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons'; 
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: 'orange' },
+          headerTitleAlign: 'center',
+        }}
+      >
+        <Stack.Screen
+          name='Home'
+          component={Home}
+          options={({ navigation }) => ({
+            title: 'My Homepage',
+            headerRight: () => (
+              <Pressable onPress={() => navigation.navigate('User')}>
+                <AntDesign name="user" size={24} color="black" />  
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen 
+        name='User' 
+        component={User}
+        options={({ navigation }) => ({
+          title: 'User Page',
+            headerRight: () => (
+              <Pressable onPress={() => navigation.navigate('Info')}>
+                <AntDesign name="info" size={24} color="black" />  
+              </Pressable>
+            ),
+          })}
+        />
+        <Stack.Screen 
+        name='Info' 
+        component={Info}
+        options={({ navigation }) => ({
+          title: 'User Page',
+            headerRight: () => (
+              <Pressable onPress={() => navigation.navigate('Home')}>
+                <AntDesign name="home" size={24} color="black" />  
+              </Pressable>
+            ),
+          })}
+        />
+      </Stack.Navigator>
+      {/* Set statusbar text color */}
+      <StatusBar style='dark'/>
+    </NavigationContainer>
+  );
+}
+
+function Home({ navigation }) {
+  return (
+    /* Example pressable in function ,can be removed */
+    <Pressable onPress={() => navigation.navigate('User')}>
+      <Text style={styles.text}>USER</Text>
+    </Pressable>
+  );
+}
+
+function User({ navigation }) {
+  return (
+    /* Example pressable in function ,can be removed */
+    <Pressable onPress={() => navigation.navigate('Info')}>
+      <Text style={styles.text}>INFO</Text>
+    </Pressable>
+  );
+}
+
+function Info({ navigation }) {
+  return (
+    /* Example pressable in function ,can be removed */
+    <Pressable onPress={() => navigation.navigate('Home')}>
+      <Text style={styles.text}>HOME</Text>
+    </Pressable>
   );
 }
 
@@ -16,5 +92,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    textAlign: 'center',
   },
 });
